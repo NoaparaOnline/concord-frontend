332a3667552df8a2abd426bab11cb57fa111973f
moizabedin31 <moiz@rippleuae.com>
2021-08-31 19:40:26 +0500

    HomepageCodeUpdate

--- src/components/FlipCardsHome/HomeCards.js
+++ src/components/FlipCardsHome/HomeCards.js
@@ -10,11 +10,10 @@ const HomeCards = ({ card }) => {
           {card.map((ob, index) => {
             return (
               <>
-              
               <div className={index==0 ? `col-lg-2 offset-1 d-none d-lg-block d-md-none col-md-4  col-sm-12 custome-card-test`:`col-lg-2 d-none d-lg-block d-md-none col-md-4  col-sm-12 custome-card-test`}>
                 <FlipCard card={ob} />
               </div>
-              <div className="custome_xs_screen d-none d-lg-none d-sm-block col-md-4 col-sm-12 col-xs-12  custome-card-test ">
+              <div className="custome_xs_screen  d-lg-none d-sm-block col-md-4 col-sm-12 col-xs-12  custome-card-test ">
                 <SingleCard card={ob} />
               </div>
               </>
