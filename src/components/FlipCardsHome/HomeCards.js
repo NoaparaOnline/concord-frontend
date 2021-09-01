3986d3a229214115e67d35a1df076610d2760a20
moizabedin31 <moiz@rippleuae.com>
2021-09-01 14:15:07 +0500

    FinalHomepage

--- src/components/FlipCardsHome/HomeCards.js
+++ src/components/FlipCardsHome/HomeCards.js
@@ -8,14 +8,14 @@ const HomeCards = ({ card }) => {
       <div className="container cards mx-1 ">
         <div className="row ">
           {card.map((ob, index) => (
-            <div key={ob.id}>
+            <React.Fragment key={ob.id}>
               <div  className={index===0 ? `col-lg-2 offset-1 d-none d-lg-block d-md-none col-md-4  col-sm-12 custome-card-test`:`col-lg-2 d-none d-lg-block d-md-none col-md-4  col-sm-12 custome-card-test`}>
                 <FlipCard card={ob} />
               </div>
               <div  className="custome_xs_screen  d-lg-none d-sm-block col-md-4 col-sm-12 col-xs-12  custome-card-test ">
                 <SingleCard card={ob} />
               </div>
-              </div>
+              </React.Fragment>
           )
           )}
         </div>
