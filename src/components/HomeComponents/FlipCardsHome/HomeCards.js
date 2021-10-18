629ff86c72d44aa2bee27e212b0d085998f632c4
Daniyal Ahmed <maaz@rippleuae.com>
2021-10-18 13:36:07 +0500

    CardFixed

--- src/components/HomeComponents/FlipCardsHome/HomeCards.js
+++ src/components/HomeComponents/FlipCardsHome/HomeCards.js
@@ -9,9 +9,9 @@ const HomeCards = ({ card }) => {
         <div className="row  ">
           {card.map((ob, index) => (
             <React.Fragment key={ob.id}>
-              <div  className={index===0  ? `col-xl-2 col-lg-4  offset-xl-1 d-none d-lg-block d-md-none  custome-card-test mb-4` 
+              <div  className={index===0  ? `col-xxl-2 col-xl-4 col-lg-4  offset-xxl-1 d-none d-lg-block d-md-none  custome-card-test mb-4` 
              
-              : `col-xl-2 col-lg-4  d-none d-lg-block d-md-none  custome-card-test mb-4`}>
+              : `col-xxl-2 col-xl-4 col-lg-4  d-none d-lg-block d-md-none  custome-card-test mb-4`}>
                 <FlipCard card={ob} />
               </div>
               <div  className="custome_xs_screen  d-lg-none d-sm-block col-md-4 col-sm-12 col-xs-12  custome-card-test mb-4">
