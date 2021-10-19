2dff38cac2d8b6d11a9b6c28c8118b5eb852c7f2
ShahzaibQadir <77103950+ShahzaibQadir@users.noreply.github.com>
2021-10-20 04:23:35 +0500

    ConcordFinish

--- src/Pages/Productsbytrade.jsx
+++ src/Pages/Productsbytrade.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { CompanyLogos } from "../components";
 import BannerWithText from "../components/ReusableComponents/BannerImgComponents/BannerImgComponents";
 import { FlipCard } from "../components";
@@ -15,7 +15,7 @@ import Dopagutbk from "../Statics/assets/TabletsFrontBack/ban-Dopagut-60-mlbk.jp
 import histaminefr from "../Statics/assets/TabletsFrontBack/Anti-histaminefr.jpg";
 import histaminebk from "../Statics/assets/TabletsFrontBack/Anti-histaminebk.jpg";
 import { Pagination } from "react-bootstrap";
-
+import ReactPaginate from 'react-paginate';
 
 
 const Products_bytrade = () => {
@@ -40,31 +40,44 @@ const Products_bytrade = () => {
   const filteredtype = (type) => {
     if (type==="All") {
       setObj(ByTrade)
+      setPageNumber(0);
+      
     }
     else if (type==="Syrup") {
       const filterd = ByTrade.filter(category => 
         category.type === "Syrup" )
       setObj(filterd);
+      setPageNumber(0);
+      
     }
     else if (type==="Tablet") {
       const filterd = ByTrade.filter(category => 
         category.type === "Tablet" )
       setObj(filterd);
+      setPageNumber(0);
+      
     }
     else if (type==="Capsule") {
       const filterd = ByTrade.filter(category => 
         category.type === "Capsule" )
       setObj(filterd);
+      setPageNumber(0);
+      
     }
     else if (type==="Hand Rub") {
       const filterd = ByTrade.filter(category => 
         category.type === "Hand Rub" )
       setObj(filterd);
+      setPageNumber(0);
+      
     }
     else if (type==="Injectables") {
       const filterd = ByTrade.filter(category => 
         category.type === "Injectables" )
       setObj(filterd);
+      setPageNumber(0);
+      
+
     }
   }
   // let paginationConfig = {
@@ -86,7 +99,34 @@ const Products_bytrade = () => {
   //   circle: true,
   //   shadow: true
   // };
-  return (
+
+  const [pageNumber, setPageNumber] = useState(0)
+  const perPage = 9;
+  const pageVisited =pageNumber * perPage;
+  
+  const displayUsers = obj
+  .slice(pageVisited , pageVisited + perPage)
+  .map((ob, index) => (
+    <React.Fragment key={ob.id}>
+      <>
+        <div className="col-lg-4 d-none d-lg-block d-md-none">
+          <FlipCard card={ob} />
+        </div>
+        <div className="col-lg-4 d-lg-none d-sm-block col-md-6 col-sm-6 col-xs-12">
+          <SingleCard card={ob} />
+        </div>
+       
+      </>
+    </React.Fragment>
+  ));
+ 
+  const pageCount = Math.ceil(obj.length/ perPage) ;
+
+  const changePage = ({selected}) => {
+    setPageNumber(selected);
+  }
+
+return (
     <div>
       <BannerWithText
         heading={"By Trade Name"}
@@ -150,7 +190,7 @@ const Products_bytrade = () => {
                   onChange={()=>filteredtype("Tablet")}
                 />
                 <label htmlFor="tablet">Tablet</label>
-                <br />
+                <br/>
                 <input
                   className="me-2"
                   type="radio"
@@ -178,19 +218,18 @@ const Products_bytrade = () => {
           <div className="col-sm-12 col-lg-9 mt-0">
             <div className="cards ">
               <div className="row">
-                {obj.map((ob, index) => (
-                  <React.Fragment key={ob.id}>
-                    <>
-                      <div className="col-lg-4 d-none d-lg-block d-md-none">
-                        <FlipCard card={ob} />
-                      </div>
-                      <div className="col-lg-4 d-lg-none d-sm-block col-md-6 col-sm-6 col-xs-12">
-                        <SingleCard card={ob} />
-                      </div>
-                     
-                    </>
-                  </React.Fragment>
-                ))}
+                {displayUsers}
+                <ReactPaginate
+                previousLabel={<i  style={{fontSize:'20px' ,color: '#0066b3'}} className="fa fa-arrow-left"></i>}
+                nextLabel={<i  style={{fontSize:'20px' ,color: '#0066b3'}} className="fa fa-arrow-right"></i>}
+                pageCount={pageCount}
+                onPageChange={changePage}
+                containerClassName={"pagination"}
+                subContainerClassName={"pages pagination"}
+                activeClassName={"active"}
+               
+                forcePage={pageNumber}
+                />
                 {/* <Pagination {...paginationConfig} /> */}
               </div>
             </div>
