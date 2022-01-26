60ecebe5bc7b22d752abc9baa39f50d501d9514d
Aun Raza <aun@rippleuae.com>
2022-01-26 18:15:44 +0500

    set facilities_product text for localization

--- src/Pages/Facilitiesproduct.jsx
+++ src/Pages/Facilitiesproduct.jsx
@@ -4,9 +4,10 @@ import bannerimg from "../Statics/assets/fac-production-ban.jpg";
 import facrd from "../Statics/assets/fac-production.jpg";
 import BannerWithText from "../components/ReusableComponents/BannerImgComponents/BannerImgComponents";
 import { Helmet } from 'react-helmet';
+import { useTranslation } from 'react-i18next';
 
 const Facilities_product = () => {
-    
+    const {t}=useTranslation('common')
    // BannerWithText Wale Ki Links Ka Object
    const LinksBan=[
     {
@@ -23,15 +24,14 @@ const Facilities_product = () => {
   return (
         <>
             <Helmet>
-        <title>Production - Concord Pharma</title>
+        <title>{t('facilities_product.helmet.prod_concord_text')}</title>
       </Helmet>
         <BannerWithText
           imgSrc={bannerimg}
-          heading={"Production"}
-          subHeading={`PRODUCTION`}
-          
-        backposit={'100% 22%'}
-        backimg={`linear-gradient(rgba(20, 20, 19, 0.8), rgba(20, 20, 19, 0.6)),url(${bannerimg})`}
+          heading={t('facilities_product.banner_with_text.production_text')}
+          subHeading={t('facilities_product.banner_with_text.production_text').toUpperCase()} 
+          backposit={'100% 22%'}
+          backimg={`linear-gradient(rgba(20, 20, 19, 0.8), rgba(20, 20, 19, 0.6)),url(${bannerimg})`}
           LinksBan={LinksBan}
           height={"400px"}
           backgroundSize={"100% 400px"}
@@ -41,21 +41,17 @@ const Facilities_product = () => {
   
         <div className="container my-5">
           <div className="row">
-         
             <div className="col-sm-12 col-lg-5">
               <div className="allborder">
               <img alt="production" src={facrd} className="zoom" width="100%" height="100%" />
               </div>
             </div>
          
-            <div
-              className="col-sm-12 col-lg-7"
-              
-            >
+            <div className="col-sm-12 col-lg-7">
               <div style={{padding:'10px',lineHeight:'1.7em',fontSize:'17px'}}>
-              Concord Pharmaceuticals Ltd is equipped with state of the art manufacturing facilities including modern technology & expertise, latest equipment & advancements.<br/><br/>
-Our production incorporates technological excellence in manufacturing, processing & packaging of various dosage forms & formulations like Tablet, Capsule, Syrup, Suspension & Injectable products.<br/><br/>
-Concord Pharmaceuticals Ltd. ensures & meets the growing requirements of cGMP regulations and other quality systems for all stage of production & in process control. The company is certified by the Quality Management System of ISO- 9001:2008 and WHO cGMP.
+              {t('facilities_product.banner_with_text.desc_1')}<br/><br/>
+              {t('facilities_product.banner_with_text.desc_2')}<br/><br/>
+              {t('facilities_product.banner_with_text.desc_3')}
               </div>
             </div>
           </div>
