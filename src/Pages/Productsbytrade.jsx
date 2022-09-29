3b156326b69dfe5e436a33edce0a35125c64cb63
Haris <haris@rippleuae.com>
2022-09-29 17:16:58 +0500

    improved product categories filter

--- src/Pages/Productsbytrade.jsx
+++ src/Pages/Productsbytrade.jsx
@@ -128,12 +128,14 @@ const Products_bytrade = (props) => {
         setPageNumber(0);
         setToggleRender(true)
 
-      }else{
-        setObj(ByTherapeutic);
-        setPageNumber(0);
-        setToggleRender(true)
-
       }
+      // { filter working properly after removing below check }
+      // else{
+      //   setObj(ByTherapeutic);
+      //   setPageNumber(0);
+      //   setToggleRender(true)
+
+      // }
     }
   };
   useEffect(() => {
